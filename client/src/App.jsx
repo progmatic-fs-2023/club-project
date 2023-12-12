ed8fd5bf10948045f24180c13145e50cb170bed4
Adam Almasi <aadika8@gmail.com>
2023-12-12 21:21:40 +0100

    nav proba

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,16 @@ import { useEffect, useState } from 'react';
 import { API_URL } from './constants';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<div>HOME</div>} />
-        <Route path="/events" element={<div>EVENTS</div>} />
-        <Route path="/services" element={<div>SERVICES</div>} />
-      </Route>
-    </Routes>
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container>
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+    </Container>
+  </Navbar>
   );
 }
 
