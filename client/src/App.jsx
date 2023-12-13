7f5e664462b94e6f6e0343ef2a5e49284cf060b0
Viktoria Kota <viktoria.kota@gmail.com>
2023-12-13 18:44:19 +0100

    add carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { API_URL } from './constants';
 import { Routes, Route } from 'react-router-dom';
-import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
+import MainCarousel from './components/MainCarousel.jsx';
+import Layout from './components/Layout.jsx';
+import NavBar from './components/NavBar.jsx';
+
 
 function App() {
   return (
@@ -15,56 +17,8 @@ function App() {
       </Route>
     </Routes> */
     <>
-      <Carousel data-bs-theme="dark" interval="5000" wrap="true" pause="false">
-        <Carousel.Item>
-          <img className="d-block w-100" src=".\src\assets\golf_1.webp" alt="First slide" />
-          <Carousel.Caption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            <div className="buttons">
-              <Button variant="secondary" size="lg">
-                Secondary
-              </Button>{' '}
-              <div className="secondary-buttons">
-                <Button variant="outline-light">Light</Button>{' '}
-                <Button variant="outline-light">Light</Button>{' '}
-              </div>
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src=".\src\assets\golf_2.jpg" alt="Second slide" />
-          <Carousel.Caption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <div className="buttons">
-              <Button variant="secondary" size="lg">
-                Secondary
-              </Button>{' '}
-              <div className="secondary-buttons">
-                <Button variant="outline-light">Light</Button>{' '}
-                <Button variant="outline-light">Light</Button>{' '}
-              </div>
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src=".\src\assets\golf_3.jpg" alt="Third slide" />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            <div className="buttons">
-              <Button variant="secondary" size="lg">
-                Secondary
-              </Button>{' '}
-              <div className="secondary-buttons">
-                <Button variant="outline-light">Light</Button>{' '}
-                <Button variant="outline-light">Light</Button>{' '}
-              </div>
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <NavBar />
+    < MainCarousel />
     </>
   );
 }
---
+++ client/src/components/MainCarousel.jsx
@@ -0,0 +1,59 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+
+function MainCarousel(){
+  return(
+  <Carousel data-bs-theme="dark" interval="300000" wrap="true" pause="false">
+  <Carousel.Item>
+    <img className="d-block w-100" src=".\src\assets\golf_1.jpg" alt="First slide" />
+    <Carousel.Caption>
+      <h5>First slide label</h5>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <div>
+        <Button variant="secondary" size="lg">
+          Secondary
+        </Button>{' '}
+        <div className="pt-2" >
+          <Button variant="outline-light">Light</Button>{' '}
+          <Button variant="outline-light">Light</Button>{' '}
+        </div>
+      </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img className="d-block w-100" src=".\src\assets\golf_2.jpg" alt="Second slide" />
+    <Carousel.Caption>
+      <h5>Second slide label</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div>
+        <Button variant="secondary" size="lg">
+          Secondary
+        </Button>{' '}
+        <div className="pt-2">
+          <Button variant="outline-light">Light</Button>{' '}
+          <Button variant="outline-light">Light</Button>{' '}
+        </div>
+      </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img className="d-block w-100" src=".\src\assets\golf_3.jpg" alt="Third slide" />
+    <Carousel.Caption>
+      <h5>Third slide label</h5>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <div>
+        <Button variant="secondary" size="lg">
+          Secondary
+        </Button>{' '}
+        <div className="pt-2">
+          <Button variant="outline-light">Light</Button>{' '}
+          <Button variant="outline-light">Light</Button>{' '}
+        </div>
+      </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+  )
+}
+
+export default MainCarousel
