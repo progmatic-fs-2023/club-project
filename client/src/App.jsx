7270eda57d6555a568719689f4d437b109a3d0cf
Viktoria Kota <viktoria.kota@gmail.com>
2023-12-13 19:13:45 +0100

    comment delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,6 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    /*     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<div>HOME</div>} />
-        <Route path="/events" element={<div>EVENTS</div>} />
-        <Route path="/services" element={<div>SERVICES</div>} />
-      </Route>
-    </Routes> */
     <>
       <NavBar />
       <MainCarousel />
