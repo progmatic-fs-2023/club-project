6d2d98df4bc6e7ccdcf099273e254dbf81cf5d10
Adam Almasi <aadika8@gmail.com>
2024-01-04 11:33:19 +0100

    [CLUB-33] create events pages

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,45 +1,10 @@
 import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import PropTypes from 'prop-types';
 import SearchBar from '../components/SearchBar';
 import AllEvents from '../components/AllEvents';
 
-const eventsList = [
-  {
-    id: 1,
-    name: 'A Tasting of Wines From Italy',
-    startTime: '2024-01-11 17:30:0.000',
-    endTime: '2024-01-11 20:30:0.000',
-    availableSeats: 0,
-    eventImg: 'src/assets/as_golf.webp',
-    details: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, cumque?',
-    moreDetails:
-      'Lorem ipsum dolor new amet consectetur, adipisicing elit. Nisi odit perferendis voluptatem recusandae enim dolore deleniti numquam, ratione vel sit accusantium amet cumque, itaque excepturi alias culpa optio nostrum ab quo velit? Laborum nulla, ullam in quaerat quis excepturi perferendis.',
-  },
-  {
-    id: 2,
-    name: 'VOICE & THE VIOLIN: Jason Peterson & LARISA Suarez',
-    startTime: '2024-01-25 19:00:0.000',
-    endTime: '2024-01-25 22:30:0.000',
-    availableSeats: 0,
-    eventImg: 'src/assets/as_golf.webp',
-    details: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, cumque?',
-    moreDetails:
-      'Lorem ipsum dolor new amet consectetur, adipisicing elit. Nisi odit perferendis voluptatem recusandae enim dolore deleniti numquam, ratione vel sit accusantium amet cumque, itaque excepturi alias culpa optio nostrum ab quo velit? Laborum nulla, ullam in quaerat quis excepturi perferendis.',
-  },
-  {
-    id: 3,
-    name: 'The Art of Living: Presentation & Book Signing with Christian Micheals',
-    startTime: '2024-01-18 13:00:0.000',
-    endTime: '2024-01-18 14:30:0.000',
-    availableSeats: 35,
-    eventImg: 'src/assets/as_golf.webp',
-    details: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, cumque?',
-    moreDetails:
-      'Lorem ipsum dolor new amet consectetur, adipisicing elit. Nisi odit perferendis voluptatem recusandae enim dolore deleniti numquam, ratione vel sit accusantium amet cumque, itaque excepturi alias culpa optio nostrum ab quo velit? Laborum nulla, ullam in quaerat quis excepturi perferendis.',
-  },
-];
-
-function Events() {
+function Events({ eventsList }) {
   const [events, setEvents] = useState(eventsList);
   const [noResults, setNoResults] = useState(false);
 
@@ -80,5 +45,9 @@ function Events() {
     </div>
   );
 }
+
+Events.propTypes = {
+  eventsList: PropTypes.string.isRequired,
+};
 
 export default Events;
